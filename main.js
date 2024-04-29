7076116bc5a26789721656d874eda648db5eee8c
KGTCreator <524Ktobin@frhsd.com>
2024-04-29 14:23:31 -0400

    Product & Description

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 const product = 'Socks'
+
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'One of the most common pieces of clothing out there, Socks are nice for keeping your feet warm, and can be very stylish. Just try not to put them under sandals.'
+        }
+    }
+})
